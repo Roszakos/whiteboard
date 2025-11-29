8ba01d9a86a9469be44befd0493d8ab674292dad
Roszako <m.roszakos@gmail.com>
2025-11-29 22:19:32 +0100

    Provided Tool and PenSettings contexts in app root

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,34 @@
 'use client';
+
 import Whiteboard from "@/src/components/Whiteboard";
+import Sidebar from "@/src/components/Sidebar/Sidebar";
+import {useReducer, useState} from "react";
+import {SetToolContext, Tool, ToolContext} from "@/src/contexts/ToolContext";
+import {PenSettingsContext, PenSettingsDispatchContext, penSettingsReducer} from "@/src/contexts/PenSettingsContext";
+
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-200 font-sans">
-        <Whiteboard />
-    </div>
-  );
+    const [tool, setTool] = useState<Tool>('pen');
+    const [penSettings, dispatch] = useReducer(
+        penSettingsReducer,
+        {
+            color: '#000000',
+            width: 15
+        },
+    );
+
+    return (
+        <ToolContext value={tool}>
+            <PenSettingsContext value={penSettings}>
+                <PenSettingsDispatchContext value={dispatch}>
+                    <div className="flex min-h-screen items-center justify-center bg-zinc-200 font-sans">
+                        <SetToolContext value={setTool}>
+                            <Sidebar/>
+                        </SetToolContext>
+                        <Whiteboard/>
+                    </div>
+                </PenSettingsDispatchContext>
+            </PenSettingsContext>
+        </ToolContext>
+    );
 }
