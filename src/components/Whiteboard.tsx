ca5617fc00e1bcdd1f16df5cdf15b35152e72a46
Roszako <m.roszakos@gmail.com>
2025-11-30 13:26:33 +0100

    Added clearing UndoneEvents on paths full clear

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -89,6 +89,7 @@ export default function Whiteboard() {
         }
 
         setEvents(prevState => [...prevState, {pathStateAfter: []}]);
+        setUndoneEvents([]);
         setPaths([]);
     }
 
